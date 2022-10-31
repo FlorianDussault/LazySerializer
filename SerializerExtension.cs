195e220bdf213b359240f30fba35cea5e18dd596
Florian Dussault <floriandussault@hotmail.com>
2022-10-31 22:25:31 +0100

    Extension Method renamed

--- SerializerExtension.cs
+++ SerializerExtension.cs
@@ -7,6 +7,6 @@
         /// </summary>
         /// <param name="obj">Object</param>
         /// <param name="path">Path</param>
-        public static void SaveSettings(this object obj, string path) => Serializer.Write(path, obj);
+        public static void Serialize(this object obj, string path) => Serializer.Write(path, obj);
     }
 }
